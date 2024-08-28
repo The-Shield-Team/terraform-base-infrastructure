4e89aeb1cb0bb7b6b2bebc358a6568253b688aef
AlejandroFerrera <aferrerah1421@gmail.com>
2024-08-28 18:47:14 -0400

    chore: :bug: remove commented out database password in main.tf

--- supabase/main.tf
+++ supabase/main.tf
@@ -1,7 +1,6 @@
 resource "supabase_project" "production" {
   organization_id = var.organization_id
   name            = "HIR"
-  //database_password = "op://The-Shield-Team/HIR_Database_Password/password"
   database_password = var.database_password
   region            = "sa-east-1"
 
